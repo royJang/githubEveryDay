cffb1bb2897b11e39220fda212650034e6534de0
zhangziliang@shining3d.com <zhangziliang@shining3d.com>
2015-11-09 17:31:21 +0800

    2015-11-9 17:31:21

---
+++ index.js
@@ -0,0 +1,43 @@
+var schedule = require("node-schedule");
+var shell = require('shelljs/global');
+var fs = require("fs");
+
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(1, 6)];
+//rule.hour = 24;
+//rule.minute = 0;
+var times = [];
+for(var i=1; i<60; i+=10){
+    times.push(i);
+}
+rule.second = times;
+
+var j = schedule.scheduleJob(rule, function(){
+    tryToCommit();
+});
+
+function tryToCommit (){
+    changeFiles(function (err){
+        try {
+            exec('git add -A');
+            exec('git commit -m "'+ now() + '"');
+            exec("git push origin master");
+        } catch (e){
+            tryToCommit();
+        }
+    })
+}
+
+function changeFiles ( callback ){
+    fs.writeFile("./file", now() , callback);
+}
+
+function now (){
+    var d = new Date();
+    return d.getFullYear() + "-"
+            + (d.getMonth() + 1) + "-"
+            + d.getDate() + " "
+            + d.getHours() + ":"
+            + d.getMinutes() + ":"
+            + d.getSeconds();
+}
